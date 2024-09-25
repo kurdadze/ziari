67a4d2f2077c3909e3d4059800dd83c90c338875
Ioseb Kurdadze <kurdadze@gmail.com>
2024-09-26 00:33:22 +0400

    add BottomNavigationView

--- app/src/main/java/com/ziarinetwork/ziari/MainActivity.kt
+++ app/src/main/java/com/ziarinetwork/ziari/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ziarinetwork.ziari
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -17,21 +18,32 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
+
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
     private lateinit var currentLocation: Location
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val locationPermissionCode = 101
+    private var density = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val display = windowManager.defaultDisplay
+        val outMetrics = DisplayMetrics()
+        display.getMetrics(outMetrics)
+
+        density = resources.displayMetrics.density
+        val dpHeight = outMetrics.heightPixels / density
+        val dpWidth = outMetrics.widthPixels / density
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         getCurrentLocationUser()
 
         BottomSheetBehavior.from(findViewById(R.id.markersInfo)).apply {
-            peekHeight = 80
+            peekHeight = 120
             this.state = BottomSheetBehavior.STATE_COLLAPSED
         }
 
@@ -75,6 +87,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val locations = listOf(
             LatLng(currentLocation.latitude, currentLocation.longitude),
+            LatLng(41.713521, 44.782219),
             LatLng(40.748817, -73.985428),
             LatLng(48.858844, 2.294351),
             LatLng(51.507351, -0.127758)
@@ -82,10 +95,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val imageUrls = listOf(
             "https://thumbs.dreamstime.com/b/bamboo-hanging-bridge-over-sea-to-tropical-island-pedestrian-remote-desert-beautiful-landscape-travel-lifestyle-wild-nature-82365255.jpg",
+            "https://i.insider.com/5f5a895be6ff30001d4e82b3?width=800&format=jpeg&auto=webp",
             "https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?cs=srgb&dl=pexels-freestockpro-3278215.jpg&fm=jpg",
             "https://i.insider.com/5f5a895be6ff30001d4e82b3?width=800&format=jpeg&auto=webp",
             "https://thumbs.dreamstime.com/b/exotic-tropical-resort-jetty-near-cancun-mexico-travel-vacations-concept-tourism-87825663.jpg"
         )
+
+        var markersWidthHeight = 0
+
+        markersWidthHeight = if(density < 3) 90 else 60
 
         for ((index, latLng) in locations.withIndex()) {
             val markerOptions = MarkerOptions().position(latLng)
@@ -96,8 +114,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             marker?.loadCustomIcon(
                 context = this,
                 imageUrl = imageUrls[index],
-                width = 50,
-                height = 50
+                width = markersWidthHeight,
+                height = markersWidthHeight
             )
 
             googleMap.setOnCameraIdleListener {
